8d4fabf6b6b64578511994ec189657630b7c37aa
Matt Ellis <mellis@odecee.com.au>
2016-11-07 09:47:55 +1100

    Java docs for Robot class

--- src/main/java/com/mattellis/robot/service/Robot.java
+++ src/main/java/com/mattellis/robot/service/Robot.java
@@ -18,37 +18,73 @@ public class Robot {
     private Integer yPosition;
     private Direction facing;
 
+    /**
+     * PLACE the toy robot on the table in position X, Y and facing NORTH, SOUTH, EAST or WEST.
+     *
+     * @param xPosition
+     * @param yPosition
+     * @param facing
+     */
     public void place(Integer xPosition, Integer yPosition, Direction facing) {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this.facing = facing;
     }
 
+    /**
+     * Check if the robot has been placed on the table
+     *
+     * @return boolean
+     */
     public boolean isPlaced() {
         return xPosition != null && yPosition != null && facing != null;
     }
 
+    /**
+     * MOVE the robot one unit forward in the direction it is currently facing
+     */
     public void move() {
         xPosition = nextXPosition();
         yPosition = nextYPosition();
     }
 
+    /**
+     * Check the next X position if a MOVE command were executed from the current position and direction
+     *
+     * @return Integer
+     */
     public Integer nextXPosition() {
         return xPosition + facing.getxMove();
     }
 
+    /**
+     * Check the next X position if a MOVE command were executed from the current position and direction
+     *
+     * @return Integer
+     */
     public Integer nextYPosition() {
         return yPosition + facing.getyMove();
     }
 
+    /**
+     * Rotate the robot 90 degrees LEFT (anti-clockwise) without changing the position of the robot.
+     */
     public void left() {
         facing = DIRECTIONS[(facing.ordinal() + DIRECTIONS.length - 1) % DIRECTIONS.length];
     }
 
+    /**
+     * Rotate the robot 90 degrees RIGHT (clockwise) without changing the position of the robot.
+     */
     public void right() {
         facing = DIRECTIONS[(facing.ordinal() + 1) % DIRECTIONS.length];
     }
 
+    /**
+     * REPORT the X, Y and F of the robot as String output
+     *
+     * @return String
+     */
     public String report() {
         return xPosition + "," + yPosition + "," + facing.name();
     }
